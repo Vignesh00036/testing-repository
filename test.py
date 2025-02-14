d1f0917e9a29032e2cbac6fb4a760e5708a290c0
Vignesh Elumalai <vigneshoffl36@gmail.com>
2025-02-14 12:48:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,8 @@ import random
 
 random_number= random.randint(1, 10)
 
-def hello(number):
-    if number==random_number:
-        print(f'You made it, the random number is {random_number}')
+for i in random_numbers:
+    if i%2==0:
+        print(*)
     else:
-        print (f'No you looser, the random number is {random_number}')
-
-hello(int(input('Enter the number: ')))
+        print(-)
